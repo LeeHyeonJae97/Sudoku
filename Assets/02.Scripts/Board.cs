8c8c8d44d17191d6f22592307c38f4ae00064afd
이현재 <qazx5024@gmail.com>
2023-04-03 11:47:56 +0900

    update

--- Assets/02.Scripts/Board.cs
+++ Assets/02.Scripts/Board.cs
@@ -7,8 +7,6 @@ public class Board : MonoBehaviour
 {
     private const int SIZE = 9;
 
-    [SerializeField] private int _tryCount;
-    [SerializeField] private int _blank;
     [SerializeField] private SlotBehaviour _slotBehaviourPrefab;
 
     private void Awake()
@@ -20,15 +18,15 @@ public class Board : MonoBehaviour
     {
         Slot[,] slots = new Slot[SIZE, SIZE];
 
-        int count = 0;
-
-        while (!Initialize() && count++ < _tryCount) { Debug.Log("Fail!"); }
-
+        if (!Generate())
+        {
+            Debug.LogError("Fail to generate sudoku puzzle");
+            return;
+        }
         Erase();
-
         Instantiate();
 
-        bool Initialize()
+        bool Generate()
         {
             for (int row = 0; row < SIZE; row++)
             {
@@ -92,64 +90,7 @@ public class Board : MonoBehaviour
 
         void Erase()
         {
-            List<Slot> blanks = new List<Slot>();
 
-            int[][] weights = new int[SIZE][];
-
-            for (int i = 0; i < weights.Length; i++)
-            {
-                weights[i] = new int[SIZE];
-
-                for (int j = 0; j < weights.Length; j++)
-                {
-                    weights[i][j] = SIZE - 1;
-                }
-            }
-
-            for (int i = 0; i < _blank; i++)
-            {
-                int row = Random(weights.Select((w) => w.Sum()).ToArray());
-                int column = Random(weights[row]);
-
-                weights[row][column] = 0;
-
-                blanks.Add(slots[row, column]);
-            }
-
-            for (int i = 0; i < blanks.Count; i++)
-            {
-                var blank1 = blanks[i];
-
-                for (int j = 0; j < blanks.Count; j++)
-                {
-                    var blank2 = blanks[j];
-
-                    if (blank1.row == blank2.row || blank1.column == blank2.column || (blank1.row / 3 == blank2.row / 3 && blank1.column / 3 == blank2.column / 3))
-                    {
-                        blank2.candidates.Add(blank1.number);
-                    }
-                }
-
-                blank1.number = 0;
-            }
-
-            int Random(int[] weights)
-            {
-                int sum = weights.Sum();
-                int num = UnityEngine.Random.Range(0, sum);
-
-                for (int j = 0; j < weights.Length; j++)
-                {
-                    if (weights[j] > 0 && num < weights[j])
-                    {
-                        return j;
-                    }
-
-                    num -= weights[j];
-                }
-
-                return -1;
-            }
         }
 
         void Instantiate()
